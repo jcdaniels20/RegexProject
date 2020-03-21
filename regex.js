488f57c05f14a5e85689cb6a6f618687b4e16dd5
Jarrod Daniels <jcdaniels@umass.edu>
2020-03-21 19:01:51 -0400

    multi match expressions

--- regex.js
+++ regex.js
@@ -1,4 +1,9 @@
-function highlight(regex, text) {
-   var regX = RegExp(document.getElementById("regularExpression").value, 'g') //global delaration of regular expression variable
-   var inputText = document.getElementById("inputText").value // declaration of var for input text
+function highlight() {
+   var regX = RegExp(document.getElementById("regularExpression").value, 'g'); //global delaration of regular expression variable
+   var inputText = document.getElementById("inputText").value; // declaration of var for input text
+   var output = input.matchAll(regX);
+
+
+   for(var ch = 0; ch < output.length; ch++) {input = input.replace(match[ch], '<span style="background-color: yellow">' + match[ch] + '</span>');}     
+   }
 }
