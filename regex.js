a30c5768e135f49c7c41e7141780f1c7e50e9435
jcdaniels20 <60196672+jcdaniels20@users.noreply.github.com>
2020-03-21 18:39:57 -0400

    adding variables

--- regex.js
+++ regex.js
@@ -1,3 +1,4 @@
 function highlight(regex, text) {
-
+   var regX = RegExp(document.getElementById("regularExpression").value, 'g') //global delaration of regular expression variable
+   var inputText = document.getElementById("inputText").value // declaration of var for input text
 }
